e8e991fd96b082ba2364a547366e1fbe0e26d560
Karrass Phiri <phirikarrass@gmail.com>
2024-01-12 05:03:33 +0200

    port changed to 8080

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
-const app = express();
- 
-app.get('/', (req, res) => {
-  res.send("Karrass Phiri");
-});
-const port = 3000; 
+const path = require('path');
 
-app.listen(process.env.PORT || port, () => {
-  console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
+const app = express();
+const port = 8080;
+
+// Middleware to serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  const indexPath = path.join(__dirname, 'public', 'index.html');
+  res.sendFile(indexPath);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
