9f8ce602a0b03a748d652071ae33def89fec64e0
Karrass Phiri <phirikarrass@gmail.com>
2024-01-27 05:14:52 +0200

    Render issue resolved

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
-
-app.use(bodyParser.json());   //This will make create user function to work
+app.use(bodyParser.json());   // This will make create user function to work
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
@@ -16,19 +15,18 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
 });
+
 app.use('/', require('./routes'));
 
-const usersRouter = require('./routes/users'); //added for Mongodb
-
-app.use('/users', usersRouter); //added for Mongodb
+const usersRouter = require('./routes/users'); // added for Mongodb
+app.use('/users', usersRouter); // added for Mongodb
 
 mongodb.initDb((err) => {
     if (err) {
         console.log(err);
-        process.exit(1); // Exit the process if database initialization fails
     } else {
         app.listen(port, () => {
-            console.log(`Database is listening and node Running on port ${port}`)
+            console.log(`Database is listening and node Running on port ${port}`);
         });
     }
 });
