ee6d185e907aedb2e505597ad8da959fe36f8455
Karrass Phiri <phirikarrass@gmail.com>
2024-01-27 20:00:30 +0200

    index.js modified

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,19 @@
-const express = require('express'); //added for frontend
-
+const express = require('express');
 const routes = require('express').Router();
-routes.use('/', require('./swagger'));
-
-const path = require('path'); //added for frontend
+const path = require('path');
 const lesson1Controller = require('../controllers/lesson1');
 
-//routes.get('/', lesson1Controller.florenceRoute);
 routes.get('/', (req, res) => {
     //#Swagger.tags=['Hello World']
     res.send('Hello World');
-})
-routes.use('/frontend', express.static(path.join(__dirname, '..', 'frontend'))); //added for frontend
+});
+
+routes.use('/frontend', express.static(path.join(__dirname, '..', 'frontend')));
+
 routes.get('/jotham', lesson1Controller.jothamRoute);
 routes.get('/jathniel', lesson1Controller.jathnielRoute);
-routes.use('/users', require('./users'));
 
+// Remove the following line, which includes the routes for 'users'
+// routes.use('/users', require('./users'));
 
 module.exports = routes;
