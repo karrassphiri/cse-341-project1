cd1449a74c263f7c1f2222a86931b64d2fee3146
Karrass Phiri <phirikarrass@gmail.com>
2024-01-28 00:25:22 +0200

    testing errors if corrected

--- helpers/validate.js
+++
@@ -1,9 +0,0 @@
-const validator = require('validator'); 
-
-const validate = (body, rules, customMessages, callback) => {
-    const validation = new validator(body, rules, customMessages);
-    validation.passes(() => callback(null, true));
-    validation.fails(() => callback(validation.errors, false));
-};
-
-module.exports = validate;
--- routes/students.js
+++ routes/students.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const studentsController = require('../controllers/students'); 
-const validation = require('../middleware/validate')
 
 router.get('/', studentsController.getAll);
 router.get('/:id', studentsController.getSingle);
-router.post('/', validation.saveStudent, studentsController.createStudent);
-router.put('/:id', validation.saveStudent, studentsController.updateStudent);
+router.post('/', studentsController.createStudent);
+router.put('/:id', studentsController.updateStudent);
 router.delete('/:id', studentsController.deleteStudent);
 
 module.exports = router;
