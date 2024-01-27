11cbda58c11305be361b7b513aa381319a3ab24e
Karrass Phiri <phirikarrass@gmail.com>
2024-01-27 20:47:54 +0200

    swagger.json modified

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,12 @@ routes.use('/frontend', express.static(path.join(__dirname, '..', 'frontend')));
 routes.get('/jotham', lesson1Controller.jothamRoute);
 routes.get('/jathniel', lesson1Controller.jathnielRoute);
 
-// Remove the following line, which includes the routes for 'users'
-// routes.use('/users', require('./users'));
+// Include routes for 'users', 'students', and 'teachers'
+const studentsRoutes = require('./students');
+const teachersRoutes = require('./teachers');
+
+// Use the respective paths for each collection
+routes.use('/students', studentsRoutes);
+routes.use('/teachers', teachersRoutes);
 
 module.exports = routes;
